90fba13ac799f614b78c81b9a1423469942994a2
Foram Patel <86473714+foram147@users.noreply.github.com>
2021-09-16 15:49:01 +0100

    Add files via upload

--- d4.js
+++ d4.js
@@ -1,6 +1,6 @@
 let books = [];
-const row = document.querySelector(".row");
-let cart = [];
+const row = document.getElementById("myGrid");
+let selectedBooks = [];
 window.onload = () => {
   fetch("https://striveschool-api.herokuapp.com/books") //get all products using API
     .then((Response) => {
@@ -10,37 +10,69 @@ window.onload = () => {
       books = book;
       book.forEach((element) => {
         //1) Display the list of items available on the page using template literals `` and .forEach
-        row.innerHTML = books.map(
-          (displayBook) => `
-            <div class="col-12 col-sm-6 col-md-4 col-lg-2">
-              <div class="card mt-2 mb-2" >
-                              <img src="${displayBook.img}" class="card-img-top w-100 h-100" alt="...">
-                        <div class="card-body">
-                                <h6 class="card-title" >${displayBook.title}</h6>
-                                <p class="card-text">${displayBook.category}</p>
-                                <a id="add" href="#" class="btn btn-primary" onclick="addToCart()">Go somewhere</a>
-                </div>
-                </div>
-                </div>
-            `
-        );
+        row.innerHTML = books.map(displayBooks(book));
       });
     })
     .catch((err) => {
       console.log(err);
     });
+};
 
-  row.querySelectorAll(".btn.btn-primary").forEach((a) => {
-    a.innerHTML = "abc";
+function filterBooks(query) {
+  // displayBooks(books.filter(b => b.title.includes(query)))
+
+  const filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(query.toLowerCase())
+  );
+
+  displayBooks(filteredBooks);
+}
+
+displayBooks = (books) => {
+  row.innerHTML = books
+    .map(
+      (book) => `
+     
+        <div class="col col-12 col-sm-6 col-md-2">
+            <div class="card  mt-2">
+                <img src="${book.img}" class="card-img-top"width=128px alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                    <p class="card-text">${book.price}</p>
+                </div>
+                
+                <div class="btn-group" role="group" aria-label="Basic example">
+                        <button id="add" type="button" class="btn btn-primary" >Add</button>
+                        <button id="skip" type="button" class="btn ">Skip</button>
+                </div>
+            </div>
+        </div>
+        </div>
+    `
+    )
+    .join("");
+  row.querySelectorAll(".btn-group .btn:nth-child(2)").forEach((btn) => {
+    btn.innerText = "Skip";
+    btn.onclick = (event) => event.currentTarget.closest(".col").remove();
+  });
+  row.querySelectorAll(".btn-group .btn:nth-child(1)").forEach((btn) => {
+    btn.addEventListener("click", function () {
+      let addBtn = btn.querySelectorAll(".btn-group .btn:nth-child(1)");
+      let selected = btn.parentElement.parentElement.parentElement;
+
+      addBtn.classList.add("selected");
+      if (addBtn.classList.contains("selected")) {
+        selectedBooks.push(selected);
+      } else {
+        let cardIndex = selectedBooks.indexOf(selected);
+        selectedBooks.splice(cardIndex, 1);
+      }
+    });
   });
 };
 
-function addToCart() {
-  //var li = document.createElement("LI");
-  var input = document.getElementById("add");
-  row.innerHTML = cart.push(input.value);
-  input.value = "";
-  row.innerHTML = a.onclick = (event) =>
-    // event.currentTarget.parentNode.parentNode.parentNode.parentNode.parentNode.remove(); // navigating upward in the dom tree to find the .col element and remove it
-    event.currentTarget.closest(".col").cart.push();
+const addBtns = document.querySelectorAll(".add");
+for (let btn of addBtns) {
 }
+
+const getCartList = document.getElementById("cartList");
